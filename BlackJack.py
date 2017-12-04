bd0823459b5be29523e11a9405574fbc5da2be1e
nicolasschmidt26 <34239036+nicolasschmidt26@users.noreply.github.com>
2017-12-04 08:11:50 -0500

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -9,10 +9,10 @@ import matplotlib.pyplot as plt
 from importer.StrategyImporter import StrategyImporter
 
 
-GAMES = 20000
-SHOE_SIZE = 6
-SHOE_PENETRATION = 0.25
-BET_SPREAD = 20.0
+GAMES = 1
+SHOE_SIZE = 1
+SHOE_PENETRATION = 0
+BET_SPREAD = 1.0
 
 DECK_SIZE = 52.0
 CARDS = {"Ace": 11, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10}
